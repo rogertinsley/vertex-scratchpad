add8bd993eac447b4827e7e8fdb2a7f89ba53c31
Roger Tinsley <mail@rogertinsley.co.uk>
2025-06-17 21:45:52 +0100

    :sunrise:

--- src/main/java/com/scratchpad/Main.java
+++ src/main/java/com/scratchpad/Main.java
@@ -1,15 +1,19 @@
 package com.scratchpad;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 
 public class Main {
     public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
+        // Set specific number of event loop threads to 4
+        VertxOptions options = new VertxOptions()
+                .setEventLoopPoolSize(4);
+        Vertx vertx = Vertx.vertx(options);
         vertx.deployVerticle(new MainVerticle())
-            .onSuccess(id -> System.out.println("MainVerticle deployed successfully"))
-            .onFailure(err -> {
-                System.err.println("Failed to deploy MainVerticle: " + err.getMessage());
-                vertx.close();
-            });
+                .onSuccess(id -> System.out.println("MainVerticle deployed successfully"))
+                .onFailure(err -> {
+                    System.err.println("Failed to deploy MainVerticle: " + err.getMessage());
+                    vertx.close();
+                });
     }
-} 
+}
--- src/main/java/com/scratchpad/ProcessVerticle.java
+++ src/main/java/com/scratchpad/ProcessVerticle.java
@@ -19,14 +19,17 @@ public class ProcessVerticle extends AbstractVerticle {
 
     @Override
     public void start(Promise<Void> startPromise) {
-        DeploymentOptions options = new DeploymentOptions();
-        options.setWorkerPoolSize(1);
+        DeploymentOptions options = new DeploymentOptions()
+                .setWorkerPoolName("process-worker-pool")
+                .setHa(false) // Set high availability to false - needs cluster mode
+                .setWorkerPoolSize(10); // Set worker pool size to 10
+                
         vertx.deployVerticle(new ProcessWorkerVerticle(), options)
-            .onSuccess(id -> {
-                System.out.println("Worker Verticle deployed successfully");
-                setupRouter(startPromise);
-            })
-            .onFailure(err -> startPromise.fail(err));
+                .onSuccess(id -> {
+                    System.out.println("Worker Verticle deployed successfully");
+                    setupRouter(startPromise);
+                })
+                .onFailure(err -> startPromise.fail(err));
     }
 
     private void setupRouter(Promise<Void> startPromise) {
@@ -34,4 +37,4 @@ public class ProcessVerticle extends AbstractVerticle {
         processHandler.setupRoutes(router);
         startPromise.complete();
     }
-} 
+}
